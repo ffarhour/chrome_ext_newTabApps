46781e5691963f07698d1be94fc6ddba3e52ab5f
ffarhour <f.farhour@gmail.com>
2016-06-21 14:40:40 +1200

    fixed problem with some unwanted tabs redirecting to apps page. added if statement to check whether the url is a new tab or not. If so then redirects it to the apps page.

--- background.js
+++ background.js
@@ -1,3 +1,27 @@
+function checkUpdate(tabID){
+    chrome.tabs.onUpdated.addListener(function(tabID,changeInfo,tab){
+        if (changeInfo.status == "complete") {
+            //alert(tab.url);
+            if(tab.url=="chrome://newtab/"){
+                chrome.tabs.update(tabID,{url:"chrome://apps/"});
+            };
+        }
+    });
+};
 chrome.tabs.onCreated.addListener(function(tab) {
-    chrome.tabs.update({url:"chrome://apps/"});
+
+    // chrome.tabs.query({
+    //     url:"chrome://newtab/"
+    //     }, function(array_of_Tabs) {
+    //         var mytab = array_of_Tabs[0];
+    //         alert(mytab);
+    //         chrome.tabs.update(mytab.id,{url:"chrome://apps/"});
+    //     }
+    // );
+
+
+    checkUpdate(tab.id);
+    //alert(mytabURL)
+
+    //setTimeout(function(){ chrome.tabs.update({url:"chrome://apps/"}); }, 1000);
 });
